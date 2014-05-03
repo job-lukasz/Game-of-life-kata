40048b9a2678684ddcdb2b36989975b103ed9202
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-04 00:31:47 +0200

    Code refactor - excluded 'killCels', 'countNeighbours' and 'initTests' methods

--- src/main/java/main/GameOfLife.java
+++ src/main/java/main/GameOfLife.java
@@ -5,33 +5,42 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class GameOfLife {
-	private Set<Cell> world;
-	private final int[][] neighboursTable= new int[][]{{-1,1},{0,1},{1,1},
-														  {-1,0},	   {1,0},
-														  {-1,-1},{0,-1},{1,-1}
-	};
+	private Set<Cell> worldBeforeTik, worldAfterTik;
+	private final int[][] neighboursTable = new int[][] { { -1, 1 }, { 0, 1 }, { 1, 1 }, { -1, 0 }, { 1, 0 }, { -1, -1 }, { 0, -1 },
+			{ 1, -1 } };
+
 	public Set<Cell> getWorld() {
-		return world;
-	}
-	public void setWorld(Set<Cell> world) {
-		this.world = world;
+		return worldBeforeTik;
 	}
 
-	public void tik(){
-		Set<Cell> newWorld = new HashSet<Cell>();
-		Iterator<Cell> worldIterator = world.iterator();
-		while(worldIterator.hasNext()){
-			int neighbours = 0;
+	public void setWorld(Set<Cell> world) {
+		this.worldBeforeTik = world;
+	}
+
+	public void tik() {
+		worldAfterTik = new HashSet<Cell>();
+		killCells();
+		worldBeforeTik = worldAfterTik;
+	}
+
+	private void killCells() {
+		Iterator<Cell> worldIterator = worldBeforeTik.iterator();
+		while (worldIterator.hasNext()) {
 			Cell cell = worldIterator.next();
-			for(int i=0;i<8;i++){
-				if(world.contains(new Cell(cell.getX()+neighboursTable[i][0],cell.getY()+neighboursTable[i][1]))){
-					neighbours++;
-				}
-			}
-			if(neighbours==2||neighbours==3){
-				newWorld.add(cell);
+			int neighbours = countNeighbours(cell);
+			if (neighbours == 2 || neighbours == 3) {
+				worldAfterTik.add(cell);
 			}
 		}
-		world = newWorld;
+	}
+
+	private int countNeighbours(Cell cell) {
+		int neighbours = 0;
+		for (int i = 0; i < 8; i++) {
+			if (worldBeforeTik.contains(new Cell(cell.getX() + neighboursTable[i][0], cell.getY() + neighboursTable[i][1]))) {
+				neighbours++;
+			}
+		}
+		return neighbours;
 	}
 }
--- src/test/java/main/GolTest.java
+++ src/test/java/main/GolTest.java
@@ -4,29 +4,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GolTest {
-	@Test
-	public void LessNeighboursThenTwo() {
-		// Arrange
-		GameOfLife game = new GameOfLife();
-		Set<Cell> beforeWorld, afterTikWorld;
+	GameOfLife game;
+	Set<Cell> beforeWorld, afterTikWorld;
+
+	@BeforeMethod
+	public void initTests() {
+		game = new GameOfLife();
 		beforeWorld = new HashSet<Cell>();
+		afterTikWorld = new HashSet<Cell>();
+	}
+
+	@Test
+	public void LessNeighboursThenTwoCellDie() {
+		// Arrange
+
 		beforeWorld.add(new Cell(0, 0));
 		// Act
 		game.setWorld(beforeWorld);
 		game.tik();
 		// Assert
-		afterTikWorld = new HashSet<Cell>();
-		Assert.assertEquals(game.getWorld().equals(afterTikWorld),true);
+		Assert.assertEquals(game.getWorld().equals(afterTikWorld), true);
 	}
+
 	@Test
 	public void TwoNeighboursCellAlive() {
 		// Arrange
-		GameOfLife game = new GameOfLife();
-		Set<Cell> beforeWorld, afterTikWorld;
-		beforeWorld = new HashSet<Cell>();
 		beforeWorld.add(new Cell(-1, 0));
 		beforeWorld.add(new Cell(0, 0));
 		beforeWorld.add(new Cell(1, 0));
@@ -34,7 +40,6 @@ public class GolTest {
 		game.setWorld(beforeWorld);
 		game.tik();
 		// Assert
-		afterTikWorld = new HashSet<Cell>();
 		afterTikWorld.add(new Cell(0, 0));
 		Assert.assertEquals(game.getWorld().equals(afterTikWorld), true);
 	}
